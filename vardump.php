c039efbd23e4c2005175e0221253eb90ae385eac
NIKHILESH SATHUA <115941024+Nikhilesh-Sathua@users.noreply.github.com>
2022-10-17 12:18:36 +0530

    Add files via upload

---
+++ vardump.php
@@ -0,0 +1,13 @@
+<?php  
+class demo  
+{  
+        private $a= "Hello CPGS ";  
+        public function display()  
+        {  
+        echo $this->a;  
+        }  
+}  
+$obj = new demo();  
+    $obj->display();  
+    var_dump($obj);  
+?>  
